b8b8e993c53cd12be55744da3c552474f8f61b0d
“WolfDen <wolfcodeden@gmail.com>
2024-04-13 14:23:51 +0200

    Some adjustments

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-let items = document.getElementsByClassName("item")
-let firstBox = document.getElementById("firstBox")
-let secondBox = document.getElementById("secondBox")
+let items = document.querySelectorAll(".item");
+let firstBox = document.getElementById("firstBox");
+let secondBox = document.getElementById("secondBox");
 
-for (item of items){
-    item.addEventListener("dragstart", function(event) {
-        let selected = event.target
-        secondBox.addEventListener("dragover", function(event){
-            event.preventDefault()
-        })
-        secondBox.addEventListener("drop", function(event){
-            secondBox.appendChild(selected)
-            selected = null
-        })
+items.forEach((item) => {
+  item.addEventListener("dragstart", (event) => {
+    let selected = event.target;
+    secondBox.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
+    secondBox.addEventListener("drop", () => {
+      secondBox.appendChild(selected);
+      selected = null;
+    });
 
-        firstBox.addEventListener("dragover", function(event){
-            event.preventDefault()
-        
-        })
-        firstBox.addEventListener("drop", function(event){
-            firstBox.appendChild(selected)
-            selected = null
-        })
-    })
-}
+    firstBox.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
+    firstBox.addEventListener("drop", () => {
+      firstBox.appendChild(selected);
+      selected = null;
+    });
+  });
+});
